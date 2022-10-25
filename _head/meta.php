f585227f205e4907acc1737c9610614c4c8e57eb
Sakumi <sakumimi10@hotmail.fr>
2022-10-26 00:13:41 +0200

    css footer

--- _head/meta.php
+++ _head/meta.php
@@ -1,3 +1,3 @@
-<meta charset="utf-8">
+<meta charset="utf_8">
 <meta name="Portfolio" content="Mon portfolio">
 <meta name="viewport" content="width=device-width, initial-scale=1">
--- _src/php/footer.php
+++ _src/php/footer.php
@@ -1,12 +1,12 @@
 
 
-<address> 
-    <h4>Me contacter</h4>
-        <p>Mail :<a class="text_white contact" href="mailto:h.fraichot@gmail.com"> h.fraichot@gmail.com</a></p>
-        <p>Tèl :<a class="text_white contact" href="tel:+33666486240"> 06.66.48.62.40</a></p>
+<address class="row justify-content-center"> 
+    <p class="col align-self-center">
+            email : <a class="text_white contact" href="mailto:h.fraichot@gmail.com"> h.fraichot@gmail.com</a><br>
+            Tèl : <a class="text_white contact" href="tel:+33666486240"> 06.66.48.62.40</a></p>
 
-        Hashley Fraichot<br>
+    <p class="col align-self-center">Hashley Fraichot<br>
         10 résidence des chatries<br>
         91540<br>
-        Mennecy
+        Mennecy</p>
 </address>
