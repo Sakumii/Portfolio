fc9bc6fbf441acba1a53ff3dd3ed9a5023393883
Sakumi <sakumimi10@hotmail.fr>
2022-10-25 18:00:47 +0200

    add base for work

---
+++ _config/database.php
@@ -0,0 +1,34 @@
+<?php
+  class connexionDB {
+    private $host    = 'localhost';
+    private $name    = 'portfolio';    
+    private $user    = 'root';       
+    private $pass    = '';       
+    private $connexion;
+    
+    function __construct($host = null, $name = null, $user = null, $pass = null){
+        if($host != null){
+            $this->host = $host;           
+            $this->name = $name;           
+            $this->user = $user;          
+            $this->pass = $pass;
+        }
+        try{
+            $this->connexion = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->name,
+                $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND =>'SET NAMES UTF8MB4', 
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+      
+        }catch (PDOException $e){
+            echo 'Erreur : Impossible de se connecter  à la BDD !';
+            die();
+        }
+    }
+      
+    public function DB(){
+        return $this->connexion;
+    }   
+  }
+
+  $DBB = new connexionDB();
+  $DB = $DBB->DB();
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+require_once ('_config/database.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Head Link -->
+        <?php
+        require_once ('_head/meta.php');
+        require_once ('_head/link.php'); 
+        ?>
+    </head>
+    <body>
+        
+    <!-- Header -->
+    <header>
+        <div class="header_container">
+            <h1>Mon portfolio</h1>
+            <nav></nav>
+        </div>
+    </header>
+    
+    
+    
+    
+    
+    
+    
+        <!-- Script Bootstrap -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
